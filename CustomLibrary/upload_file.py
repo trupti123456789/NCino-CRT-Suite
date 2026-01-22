3b5cabeaf4ead5cbc7e10a93d6d2c91c26d24bce
Trupti  Shantaram Chaudhary <trupti.c@cloudfulcrum.com>
2026-01-22 13:22:20 +0000

    ssssss

--- CustomLibrary/upload_file.py
+++ CustomLibrary/upload_file.py
@@ -1,26 +1,17 @@
 from robot.api.deco import library, keyword
-from selenium.webdriver.common.by import By
+from robot.libraries.BuiltIn import BuiltIn
 import os
 
 @library
 class UploadFile:
 
-    @keyword("Upload File Using Driver")
-    def upload_file_using_driver(self, driver, file_path, upload_button_xpath=None):
-        """
-        Upload file using driver passed from Robot
-
-        Arguments:
-        - driver : WebDriver instance from CRT
-        - file_path : Absolute file path
-        - upload_button_xpath (optional) : XPath of upload button
-        """
+    @keyword("Upload File")
+    def upload_file(self, file_input_xpath, file_path, upload_button_xpath=None):
 
         if not os.path.exists(file_path):
             raise Exception(f"File not found: {file_path}")
 
         if upload_button_xpath:
-            driver.find_element(By.XPATH, upload_button_xpath).click()
+            BuiltIn().run_keyword("Click Element", upload_button_xpath)
 
-        file_input = driver.find_element(By.XPATH, "//input[@type='file']")
-        file_input.send_keys(file_path)
+        BuiltIn().run_keyword("Choose File", file_input_xpath, file_path)
